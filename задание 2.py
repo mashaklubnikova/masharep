ee2d61ba3acda95ce4d702990246bc8ec2c55cf2
mashaklubnikova <159928202+mashaklubnikova@users.noreply.github.com>
2025-01-13 14:04:18 +0300

    лабораторная 1 перезачет

--- задание 2.py
+++ задание 2.py
@@ -1,41 +1,87 @@
-from task_1 import Book, BankAccount, SocialNetworkPost
+from task_1 import Table, Tree, Facebook
 
 if __name__ == "__main__":
+    table = Table("wood", 1.0, 1.5)
+    tree = Tree("pine", 10, 5.0)
+    facebook = Facebook("Facebook", 100000, True)
 
- book = Book("Python для продвинутых", "Сергей Сергеев", 350)
- account = BankAccount("Анна Иванова", 1000.0)
- post = SocialNetworkPost("Дмитрий Сидоров", "Замечательный день!")
+    print("Объекты созданы успешно")
 
- print(book.get_info())
- print(account.deposit(200.0))
- print(post.display_post())
+    print("Начало проверки методов с валидацией")
+    try:
+        table.change_height("a")
+    except TypeError as e:
+        print(f"Ошибка: {e}")
 
- try:
-    # Вызываем метод read_chapter с некорректным номером главы
-    book.read_chapter(100)  # Больше чем предполагается глав по умолчанию
- except ValueError as e:
-    print(f"Ошибка при чтении главы: {e}")
+    try:
+      table.change_height(-1.0)
+    except TypeError as e:
+      print(f"Ошибка: {e}")
 
- try:
-    # Вызываем метод deposit с отрицательной суммой
-    account.deposit(-50.0)
- except ValueError as e:
-    print(f"Ошибка при внесении средств: {e}")
+    try:
+      tree.grow("abc")
+    except TypeError as e:
+        print(f"Ошибка: {e}")
 
- try:
-    # Вызываем метод withdraw с суммой, превышающей баланс
-    account.withdraw(2000.0)
- except ValueError as e:
-    print(f"Ошибка при снятии средств: {e}")
+    try:
+      tree.grow(-1)
+    except ValueError as e:
+        print(f"Ошибка: {e}")
 
- try:
-    # Создаем книгу с отрицательным количеством страниц
-    bad_book = Book("Плохая книга", "Неизвестный автор", -10)
- except ValueError as e:
-    print(f"Ошибка при создании книги: {e}")
+    try:
+      tree.get_carbon_storage("abc")
+    except TypeError as e:
+       print(f"Ошибка: {e}")
 
- try:
-    # Создаем счет с отрицательным балансом
-    bad_account = BankAccount("Нехороший клиент", -500.0)
- except ValueError as e:
-     print(f"Ошибка при создании счета: {e}")
+    try:
+      tree.get_carbon_storage(-1)
+    except ValueError as e:
+       print(f"Ошибка: {e}")
+
+    try:
+        facebook.add_users("abc")
+    except TypeError as e:
+        print(f"Ошибка: {e}")
+
+    try:
+        facebook.add_users(-1)
+    except ValueError as e:
+        print(f"Ошибка: {e}")
+
+
+
+    print("Начало проверки конструкторов с невалидными аргументами")
+
+    try:
+      table1 = Table(1, 1.0, 1.5)
+    except TypeError as e:
+       print(f"Ошибка: {e}")
+    try:
+      table1 = Table("wood", -1.0, 1.5)
+    except ValueError as e:
+      print(f"Ошибка: {e}")
+    try:
+      table1 = Table("wood", 1.0, -1.5)
+    except ValueError as e:
+        print(f"Ошибка: {e}")
+    try:
+      tree1 = Tree(1, 10, 5.0)
+    except TypeError as e:
+       print(f"Ошибка: {e}")
+    try:
+      tree1 = Tree("pine", -10, 5.0)
+    except ValueError as e:
+       print(f"Ошибка: {e}")
+    try:
+      tree1 = Tree("pine", 10, -5.0)
+    except ValueError as e:
+       print(f"Ошибка: {e}")
+    try:
+      facebook1 = Facebook(1, 10000, True)
+    except TypeError as e:
+      print(f"Ошибка: {e}")
+    try:
+      facebook1 = Facebook("Facebook", -10000, True)
+    except ValueError as e:
+      print(f"Ошибка: {e}")
+
